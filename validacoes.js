c5828fdd5c6fda61338fdeadfca226b76b7db686
Lucas Reis <33505633+Lucasreis1994@users.noreply.github.com>
2018-04-16 23:13:45 -0300

    Update validacoes.js

--- validacoes.js
+++ validacoes.js
@@ -1,16 +1,34 @@
-function validaObrigatorios() {
-	$(".obrigatorio").each(function(){
-        // alert($(this).val());
-        if (this.value == '') {
-        	mensagem = 'Favor preencher os campos obrigatórios';
-        	tipo = 'error';
-        	$(this).css('border','1px solid #ff0000');
-        }else{
-        	mensagem = 'Sucesso';
-        	tipo = 'success';
-        	$(this).css('border','1px solid green');
+function validaObrigatorios(idFormulario) {
+  var contaVazios = 0;
+  $(".obrigatorio").each(function(){
+      if (this.value == '') {
+        contaVazios++;
+        $(this).css('border','1px solid #ff0000');
+      }else{
+        $(this).css('border','1px solid green');
+      }
+  });
+
+  if (contaVazios > 0) {
+    swal({
+      title: "Atenção!",
+      text: "Você precisa preencher todos os campos obrigatórios!",
+      type: "warning"
+    });
+    return false;
+  }else{
+    // Envia arquivos via AJAX para o Back-end	  
+    var dados = $('form[name=formfiltro]').serialize(); // pega tudo o que está escrito no formulário, cada campo.
+    
+    $.ajax({
+        type : 'POST',
+        url : 'suaURL.php', // página para qual quer enviar os dados do formulário.
+        cache : false,
+        data : dados,
+        beforeSend : function() {
+        },
+        success : function(result) {
         }
     });
-
-    swal("Atenção!", mensagem, tipo);
+  }
 }
